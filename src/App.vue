9877a8511b4631519b91077f6ce2cefa3b86a92b
Shanaka Prince <shanakaprince@gmail.com>
2025-04-25 16:06:22 +0530

    modified App.vue in src

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
     <p>Welcome {{ message }}</p>
-    <p v-text="greet"></p>
+    <p v-if="num === 0">Ha Ha Ha</p>
+    <p v-else-if="num > 0">Number is positive</p>
+    <p v-else>Number is not zero</p>
+    <p v-html="safeHack"></p>
+    <button v-bind:disabled="isDisabled">Bind Button</button>
+  </div>
+
+  <div v-show="showElement">
+    <p>Hello world</p>
   </div>
 </template>
 
@@ -12,7 +20,16 @@ export default {
     return {
       message: "Hello Vue!",
       greet: "Greet you!",
+      num: 10,
+      safeHack: `<a href="#" onclick="alert("Hey You!")>Click me</a>`,
+      isDisabled: false,
+      showElement: true,
     };
+  },
+  methods: {
+    alertUser() {
+      alert("You are alerted");
+    },
   },
 };
 </script>
