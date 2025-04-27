d3873d0096b2663ed46844f84fbfdfaaf4e47f61
Shanaka Prince <shanakaprince@gmail.com>
2025-04-28 01:52:39 +0530

    added PopUp.vue to components

---
+++ src/components/PopUp.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>This is the popup</h2>
+    <button>Close popup</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "PopUp",
+};
+</script>
+<style></style>
